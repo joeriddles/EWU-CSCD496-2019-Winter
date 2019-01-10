3009ece52078a74fba43903ea86765dd7d7c649f
joeriddles <joeriddles10@gmail.com>
2019-01-10 14:56:27 -0800

    Add SecretSanta proj and SecretSantaTests proj

---
+++ SecretSanta/test/SecretSanta.Domain.Tests/Models/UserTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecretSanta.Domain.Models;
+
+namespace SecretSanta.Domain.Tests.Models
+{
+	[TestClass]
+	public class UserTests
+	{
+		[TestMethod]
+		public void CreateUser()
+		{
+			User user = new User { FirstName = "Inigo", LastName = "Montoya" };
+			Assert.AreEqual("Inigo", user.FirstName);
+			Assert.AreEqual("Montoya", user.LastName);
+		}
+	}
+}
