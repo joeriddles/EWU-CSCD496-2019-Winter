46a7bc5c5c60500a23a27b8c1e0aa31dad4922fb
joeriddles <joeriddles10@gmail.com>
2019-01-17 13:26:13 -0800

    Add UserGroup model tests

--- SecretSanta/test/SecretSanta.Domain.Tests/Models/UserGroupTests.cs
+++ SecretSanta/test/SecretSanta.Domain.Tests/Models/UserGroupTests.cs
@@ -1,10 +1,29 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecretSanta.Domain.Models;
 
 namespace SecretSanta.Domain.Tests.Models
 {
 	[TestClass]
 	public class UserGroupTests
 	{
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void UserGroup_CreateUserGroupWithNull_ExpectException()
+		{
+			UserGroup userGroup = new UserGroup(null, null);
+		}
 
+		[TestMethod]
+		public void UserGroup_CreateUserGroup_Success()
+		{
+			User user = new User("Inigo", "Montoya");
+			Group group = new Group(".NET User group");
+
+			UserGroup userGroup = new UserGroup(user, group);
+			Assert.AreEqual("Inigo", userGroup.User.FirstName);
+			Assert.AreEqual("Montoya", userGroup.User.LastName);
+			Assert.AreEqual(".NET User group", userGroup.Group.Title);
+		}
 	}
 }
