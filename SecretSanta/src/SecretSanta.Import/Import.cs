5f50f858f219e4f9d972517c588e253516a1128e
joeriddles <joeriddles10@gmail.com>
2019-01-22 21:36:32 -0800

    Add SecretSanta.Import and SecretSanta.Import.Tests

---
+++ SecretSanta/src/SecretSanta.Import/Import.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using SecretSanta.Domain.Models;
+
+namespace SecretSanta.Import
+{
+	public class Import
+	{
+		public string Filename { get; }
+
+		public Import(string filename)
+		{
+			if (filename is null)
+				throw new ArgumentNullException(nameof(filename));
+			if (!File.Exists(filename))
+				throw new ArgumentException(nameof(filename), "File does not exist.");
+			if (!File.ReadLines(filename).Any())
+				throw new ArgumentException(nameof(filename), "File does not contain header.");
+
+			Filename = filename;
+		}
+
+		public User ReadHeader()
+		{
+			string header = File.ReadLines(Filename).First();
+			string[] headerArray = header.Split(':');
+			if (headerArray.Length != 2 || !headerArray[0].Equals("Name"))
+				throw new ArgumentException(nameof(Filename), "Header formatted incorrectly.");
+
+			string name = headerArray[1].Trim();
+			string[] nameFormat = name.Split(',', 2);
+
+			if (nameFormat.Length == 1)
+			{
+				string[] splitName = nameFormat[0].Split();
+				return new User(splitName[0].Trim(), splitName[1].Trim()); // <FirstName> <LastName>
+			}
+			if (nameFormat.Length == 2)
+				return new User(nameFormat[1].Trim(), nameFormat[0].Trim()); // <LastName>, <FirstName>
+			throw new ArgumentException(nameof(Filename), "Header name formatted incorrectly.");
+		}
+
+		public List<Gift> ReadBody(User user)
+		{
+			if (user is null)
+				throw new ArgumentNullException(nameof(user), "Cannot add gifts before user has been created.");
+
+			List<Gift> gifts = new List<Gift>();
+			IEnumerable<string> body = File.ReadLines(Filename).Skip(1);
+			foreach (var line in body.Where(line => line.Trim() != ""))
+				gifts.Add(new Gift(line.Trim(), "", user.Id));
+
+			return gifts;
+		}
+	}
+}
---
+++ SecretSanta/test/SecretSanta.Import.Tests/ImportTests.cs
@@ -0,0 +1,164 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecretSanta.Domain.Models;
+
+namespace SecretSanta.Import.Tests
+{
+	[TestClass]
+	public class ImportTests
+	{
+		[ClassInitialize]
+		public static void ClassInitialize(TestContext testContext)
+		{
+			//should always cleanup at start and at__tempt cleanup at the end
+			if (File.Exists("__temp.txt"))
+				File.Delete("__temp.txt");
+			if (File.Exists("__temp2.txt"))
+				File.Delete("__temp2.txt");
+
+			File.WriteAllLines("__temp.txt", new[]{ "Name: John Smith", "Racecar", "Xbox" }, Encoding.UTF8);
+			File.WriteAllLines("__temp2.txt", new []{"Name: Smith, John", "Racecar", "Xbox" }, Encoding.UTF8);
+		}
+
+		[ClassCleanup]
+		public static void ClassCleanup()
+		{
+			if (File.Exists("__temp.txt"))
+				File.Delete("__temp.txt");
+
+			Assert.IsFalse(File.Exists("__temp.txt"));
+			Assert.IsFalse(File.Exists("__temp2.txt"));
+			Assert.IsFalse(File.Exists("__empty.txt"));
+		}
+
+		[TestInitialize]
+		public void TestInitialize()
+		{
+			if (File.Exists("__empty.txt"))
+				File.Delete("__empty.txt");
+		}
+
+		[TestCleanup]
+		public void TestCleanup()
+		{
+			if (File.Exists("__empty.txt"))
+				File.Delete("__empty.txt");
+		}
+
+		[TestMethod]
+		public void InitializeAndCleanupAreWorking()
+		{
+			Assert.IsFalse(File.Exists("__empty.txt"));
+			Assert.IsTrue(File.Exists("__temp.txt"));
+			Assert.IsTrue(File.Exists("__temp2.txt"));
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void Import_filenameIsNull_ThrowsException()
+		{
+			Import import = new Import(null);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void Import_fileDoesNotExist_ThrowsException()
+		{
+			Import import = new Import("__thisfiledontexist.fake");
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void Import_fileIsEmpty_ThrowsException()
+		{
+			File.WriteAllLines("__empty.txt", new List<string>());
+			Import import = new Import("__empty.txt");
+		}
+
+		[TestMethod]
+		public void Import_fileExists_Success()
+		{
+			Import import = new Import("__temp.txt");
+			Assert.AreEqual("__temp.txt", import.Filename);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void ReadHead_FormattedIncorrectly_ThrowsException()
+		{
+			File.WriteAllLines("__empty.txt", new List<string>(){"No names here"});
+			Import import = new Import("__empty.txt");
+			import.ReadHeader();
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void ReadHead_NameFormattedIncorrectly_ThrowsException()
+		{
+			File.WriteAllLines("__empty.txt", new List<string>() { "NotName: John Smith" });
+			Import import = new Import("__empty.txt");
+			import.ReadHeader();
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void ReadHead_NameIsEmpty_ThrowsExcption()
+		{
+			File.WriteAllLines("__empty.txt", new List<string>() { "NotName: " });
+			Import import = new Import("__empty.txt");
+			import.ReadHeader();
+		}
+
+		[TestMethod]
+		public void ReadHead_FirstNameLastName_Success()
+		{
+			Import import = new Import("__temp.txt");
+			User user = import.ReadHeader();
+			Assert.AreEqual("John", user.FirstName);
+			Assert.AreEqual("Smith", user.LastName);
+		}
+
+		[TestMethod]
+		public void ReadHead_LastName_FirstName_Success()
+		{
+			Import import = new Import("__temp2.txt");
+			User user = import.ReadHeader();
+			Assert.AreEqual("John", user.FirstName);
+			Assert.AreEqual("Smith", user.LastName);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void ReadBody_NullUser_ThrowsException()
+		{
+			Import import = new Import("__temp.txt");
+			import.ReadBody(null);
+		}
+
+		[TestMethod]
+		public void ReadBody_NoGifts_EmptyList()
+		{
+			File.WriteAllLines("__empty.txt", new List<string>() { "Name: John Smith" });
+			Import import = new Import("__empty.txt");
+			User user = import.ReadHeader();
+			List<Gift> gifts = import.ReadBody(user);
+			Assert.IsNotNull(gifts);
+			Assert.AreEqual(0, gifts.Count);
+		}
+
+		[TestMethod]
+		public void ReadBody_HasGifts_Success()
+		{
+			Import import = new Import("__temp.txt");
+			User user = import.ReadHeader();
+			List<Gift> gifts = import.ReadBody(user);
+			Assert.AreEqual(2, gifts.Count);
+			Assert.AreEqual("Racecar", gifts[0].Title);
+			Assert.AreEqual("Xbox", gifts[1].Title);
+			Assert.AreEqual(user.Id, gifts[0].UserId);
+		}
+	}
+}
