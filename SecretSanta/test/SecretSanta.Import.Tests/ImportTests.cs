4b68af56d32e1ba5d2a3ebf88f3825b10d184447
joeriddles <joeriddles10@gmail.com>
2019-01-24 14:17:11 -0800

    Add use of temp directory for test files

--- SecretSanta/test/SecretSanta.Import.Tests/ImportTests.cs
+++ SecretSanta/test/SecretSanta.Import.Tests/ImportTests.cs
@@ -10,48 +10,50 @@ namespace SecretSanta.Import.Tests
 	[TestClass]
 	public class ImportTests
 	{
+		private static readonly string TempDirectory = Path.GetTempPath();
+
 		[ClassInitialize]
 		public static void ClassInitialize(TestContext testContext)
 		{
 			//should always cleanup at start and at__tempt cleanup at the end
-			if (File.Exists("__temp.txt"))
-				File.Delete("__temp.txt");
-			if (File.Exists("__temp2.txt"))
-				File.Delete("__temp2.txt");
+			if (File.Exists($"{TempDirectory}/temp.txt"))
+				File.Delete($"{TempDirectory}/temp.txt");
+			if (File.Exists($"{TempDirectory}/temp2.txt"))
+				File.Delete($"{TempDirectory}/temp2.txt");
 
-			File.WriteAllLines("__temp.txt", new[]{ "Name: John Smith", "Racecar", "Xbox" }, Encoding.UTF8);
-			File.WriteAllLines("__temp2.txt", new []{"Name: Smith, John", "Racecar", "Xbox" }, Encoding.UTF8);
+			File.WriteAllLines($"{TempDirectory}/temp.txt", new[]{ "Name: John Smith", "Racecar", "Xbox" }, Encoding.UTF8);
+			File.WriteAllLines($"{TempDirectory}/temp2.txt", new []{"Name: Smith, John", "Racecar", "Xbox" }, Encoding.UTF8);
 		}
 
 		[ClassCleanup]
 		public static void ClassCleanup()
 		{
-			if (File.Exists("__temp.txt"))
-				File.Delete("__temp.txt");
-			if (File.Exists("__temp2.txt"))
-				File.Delete("__temp2.txt");
+			if (File.Exists($"{TempDirectory}/temp.txt"))
+				File.Delete($"{TempDirectory}/temp.txt");
+			if (File.Exists($"{TempDirectory}/temp2.txt"))
+				File.Delete($"{TempDirectory}/temp2.txt");
 		}
 
 		[TestInitialize]
 		public void TestInitialize()
 		{
-			if (File.Exists("__temp3.txt"))
-				File.Delete("__temp3.txt");
+			if (File.Exists($"{TempDirectory}/temp3.txt"))
+				File.Delete($"{TempDirectory}/temp3.txt");
 		}
 
 		[TestCleanup]
 		public void TestCleanup()
 		{
-			if (File.Exists("__temp3.txt"))
-				File.Delete("__temp3.txt");
+			if (File.Exists($"{TempDirectory}/temp3.txt"))
+				File.Delete($"{TempDirectory}/temp3.txt");
 		}
 
 		[TestMethod]
 		public void InitializeAndCleanupAreWorking()
 		{
-			Assert.IsFalse(File.Exists("__temp3.txt"));
-			Assert.IsTrue(File.Exists("__temp.txt"));
-			Assert.IsTrue(File.Exists("__temp2.txt"));
+			Assert.IsFalse(File.Exists($"{TempDirectory}/temp3.txt"));
+			Assert.IsTrue(File.Exists($"{TempDirectory}/temp.txt"));
+			Assert.IsTrue(File.Exists($"{TempDirectory}/temp2.txt"));
 		}
 
 		[TestMethod]
@@ -65,30 +67,30 @@ namespace SecretSanta.Import.Tests
 		[ExpectedException(typeof(ArgumentException))]
 		public void Import_fileDoesNotExist_ThrowsArgumentException()
 		{
-			Import import = new Import("__thisfiledontexist.fake");
+			Import import = new Import($"{TempDirectory}/thisfiledontexist.fake");
 		}
 
 		[TestMethod]
 		[ExpectedException(typeof(ArgumentException))]
 		public void Import_fileIsEmpty_ThrowsArgumentException()
 		{
-			File.WriteAllLines("__temp3.txt", new List<string>());
-			Import import = new Import("__temp3.txt");
+			File.WriteAllLines($"{TempDirectory}/temp3.txt", new List<string>());
+			Import import = new Import($"{TempDirectory}/temp3.txt");
 		}
 
 		[TestMethod]
 		public void Import_fileExists_Success()
 		{
-			Import import = new Import("__temp.txt");
-			Assert.AreEqual("__temp.txt", import.Filename);
+			Import import = new Import($"{TempDirectory}/temp.txt");
+			Assert.AreEqual($"{TempDirectory}/temp.txt", import.Filename);
 		}
 
 		[TestMethod]
 		[ExpectedException(typeof(ArgumentException))]
 		public void ReadHead_FormattedIncorrectly_ThrowsArgumentException()
 		{
-			File.WriteAllLines("__temp3.txt", new List<string>(){"No names here"});
-			Import import = new Import("__temp3.txt");
+			File.WriteAllLines($"{TempDirectory}/temp3.txt", new List<string>(){"No names here"});
+			Import import = new Import($"{TempDirectory}/temp3.txt");
 			import.ReadHeader();
 		}
 
@@ -96,8 +98,8 @@ namespace SecretSanta.Import.Tests
 		[ExpectedException(typeof(ArgumentException))]
 		public void ReadHead_NameFormattedIncorrectly_ThrowsArgumentException()
 		{
-			File.WriteAllLines("__temp3.txt", new List<string>() { "NotName: John Smith" });
-			Import import = new Import("__temp3.txt");
+			File.WriteAllLines($"{TempDirectory}/temp3.txt", new List<string>() { "NotName: John Smith" });
+			Import import = new Import($"{TempDirectory}/temp3.txt");
 			import.ReadHeader();
 		}
 
@@ -105,15 +107,15 @@ namespace SecretSanta.Import.Tests
 		[ExpectedException(typeof(ArgumentException))]
 		public void ReadHead_NameIsEmpty_ThrowsArgumentException()
 		{
-			File.WriteAllLines("__temp3.txt", new List<string>() { "NotName: " });
-			Import import = new Import("__temp3.txt");
+			File.WriteAllLines($"{TempDirectory}/temp3.txt", new List<string>() { "NotName: " });
+			Import import = new Import($"{TempDirectory}/temp3.txt");
 			import.ReadHeader();
 		}
 
 		[TestMethod]
 		public void ReadHead_FirstNameLastName_Success()
 		{
-			Import import = new Import("__temp.txt");
+			Import import = new Import($"{TempDirectory}/temp.txt");
 			User user = import.ReadHeader();
 			Assert.AreEqual("John", user.FirstName);
 			Assert.AreEqual("Smith", user.LastName);
@@ -122,7 +124,7 @@ namespace SecretSanta.Import.Tests
 		[TestMethod]
 		public void ReadHead_LastName_FirstName_Success()
 		{
-			Import import = new Import("__temp2.txt");
+			Import import = new Import($"{TempDirectory}/temp2.txt");
 			User user = import.ReadHeader();
 			Assert.AreEqual("John", user.FirstName);
 			Assert.AreEqual("Smith", user.LastName);
@@ -135,8 +137,8 @@ namespace SecretSanta.Import.Tests
 		[DataRow("Name:   John   Smith   ")]
 		public void ReadHead_Spaces_FirstName_LastName_Success(string fileHeader)
 		{
-			File.WriteAllLines("__temp3.txt", new List<string>() { fileHeader });
-			Import import = new Import("__temp3.txt");
+			File.WriteAllLines($"{TempDirectory}/temp3.txt", new List<string>() { fileHeader });
+			Import import = new Import($"{TempDirectory}/temp3.txt");
 			User user = import.ReadHeader();
 			Assert.AreEqual("John", user.FirstName);
 			Assert.AreEqual("Smith", user.LastName);
@@ -146,15 +148,15 @@ namespace SecretSanta.Import.Tests
 		[ExpectedException(typeof(ArgumentNullException))]
 		public void ReadBody_NullUser_ThrowsException()
 		{
-			Import import = new Import("__temp.txt");
+			Import import = new Import($"{TempDirectory}/temp.txt");
 			import.ReadBody(null);
 		}
 
 		[TestMethod]
 		public void ReadBody_NoGifts_EmptyList()
 		{
-			File.WriteAllLines("__temp3.txt", new List<string>() { "Name: John Smith" });
-			Import import = new Import("__temp3.txt");
+			File.WriteAllLines($"{TempDirectory}/temp3.txt", new List<string>() { "Name: John Smith" });
+			Import import = new Import($"{TempDirectory}/temp3.txt");
 			User user = import.ReadHeader();
 			List<Gift> gifts = import.ReadBody(user);
 			Assert.IsNotNull(gifts);
@@ -164,7 +166,7 @@ namespace SecretSanta.Import.Tests
 		[TestMethod]
 		public void ReadBody_HasGifts_Success()
 		{
-			Import import = new Import("__temp.txt");
+			Import import = new Import($"{TempDirectory}/temp.txt");
 			User user = import.ReadHeader();
 			List<Gift> gifts = import.ReadBody(user);
 			Assert.AreEqual(2, gifts.Count);
@@ -176,8 +178,8 @@ namespace SecretSanta.Import.Tests
 		[TestMethod]
 		public void ReadBody_HasGiftsAndBlankLines_Success()
 		{
-			File.WriteAllLines("__temp.txt", new []{"Name: John Smith", "", "Racecar", "", "Xbox", ""});
-			Import import = new Import("__temp.txt");
+			File.WriteAllLines($"{TempDirectory}/temp.txt", new []{"Name: John Smith", "", "Racecar", "", "Xbox", ""});
+			Import import = new Import($"{TempDirectory}/temp.txt");
 			User user = import.ReadHeader();
 			List<Gift> gifts = import.ReadBody(user);
 			Assert.AreEqual(2, gifts.Count);
