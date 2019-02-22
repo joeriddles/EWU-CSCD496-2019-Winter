c95cea8a05c767766125dfe5d5ee4a3a8a169626
joeriddles <joeriddles10@gmail.com>
2019-02-21 16:53:24 -0800

    Add code from class

---
+++ SecretSanta/src/SecretSanta.Web/Startup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SecretSanta.Web
+{
+	public class Startup
+	{
+		private IConfiguration Configuration { get; }
+		public Startup(IConfiguration configuration) => Configuration = configuration;
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc();
+			services.AddHttpClient("SecretSantaApi", config =>
+			{
+				config.BaseAddress = new Uri(Configuration.GetSection("SecretSantaApi").GetValue<String>("BaseUri"));
+			});
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseStaticFiles();
+
+			app.UseMvcWithDefaultRoute();
+		}
+	}
+}
